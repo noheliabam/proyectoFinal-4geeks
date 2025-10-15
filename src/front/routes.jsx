9d381d549e5f7696e8e8fe9393045880c110e0a9
Edusanba <edu.santiagoba@hotmail.com>
2025-10-15 21:55:10 +0200

    feat: Landingpage

--- src/front/routes.jsx
+++ src/front/routes.jsx
@@ -12,6 +12,7 @@ import { Demo } from "./pages/Demo";
 import { Movies } from "./pages/Movies";
 import { Register } from "./pages/Register";
 import { Login } from "./pages/Login";
+import { Landing } from "./pages/Landing";
 
 export const router = createBrowserRouter(
     createRoutesFromElements(
@@ -25,7 +26,7 @@ export const router = createBrowserRouter(
       <Route path="/" element={<Layout />} errorElement={<h1>Not found!</h1>} >
 
         {/* Nested Routes: Defines sub-routes within the BaseHome component. */}
-        <Route path= "/" element={<Home />} />
+        <Route path= "/" element={<Landing />} />
         <Route path="/single/:theId" element={ <Single />} />  {/* Dynamic route for single items */}
         <Route path="/demo" element={<Demo />} />
         <Route path="/peliculas" element={<Movies />} />
